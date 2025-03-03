96c942b903ececcffca486940b2986d3ee61c727
saiyadav7702 <kadari.saiyadav01@gmail.com>
2025-03-03 13:36:19 +0000

    hfjtfj

--- agecalculator.py
+++ agecalculator.py
@@ -5,6 +5,6 @@ b=int(input("enter your birth month"))
 c=int(input("enter your birth day"))
 u=int(input("enter your present day"))
 years=r-a
-months=12-b+t
+months=12-b+t 
 days=c
 print (years, months, days)
